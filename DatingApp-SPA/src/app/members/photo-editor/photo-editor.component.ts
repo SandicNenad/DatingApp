f568deb679c35a40ad663fcca1a6485d28483c2f
Nenad Sandic <Nenad.Sandic@vanderlande.com>
2021-02-08 10:30:32 +0100

    Chapter 11 part 8&9: Adding a 3rd party file uploader, Configuring the 3rd party file uploader

--- DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
+++ DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Photo } from 'src/app/_models/photo';
+import { FileUploader } from 'ng2-file-upload';
+import { environment } from 'src/environments/environment';
+import { AuthService } from 'src/app/_services/auth.service';
 
 @Component({
   selector: 'app-photo-editor',
@@ -8,10 +11,71 @@ import { Photo } from 'src/app/_models/photo';
 })
 export class PhotoEditorComponent implements OnInit {
   @Input() photo: Photo[];
+  uploader: FileUploader;
+  hasBaseDropZoneOver = false;
+  response: string;
+  baseUrl = environment.apiUrl;
 
-  constructor() { }
+  constructor(private authService: AuthService) {
+    // this.uploader = new FileUploader({
+    //   url: URL,
+    //   disableMultipart: true, // 'DisableMultipart' must be 'true' for formatDataFunction to be called.
+    //   formatDataFunctionIsAsync: true,
+    //   formatDataFunction: async (item) => {
+    //     return new Promise( (resolve, reject) => {
+    //       resolve({
+    //         name: item._file.name,
+    //         length: item._file.size,
+    //         contentType: item._file.type,
+    //         date: new Date()
+    //       });
+    //     });
+    //   }
+    // });
 
-  ngOnInit() {
+    // this.hasBaseDropZoneOver = false;
+    // this.hasAnotherDropZoneOver = false;
+
+    // this.response = '';
+
+    // this.uploader.response.subscribe( res => this.response = res );
   }
 
+  // tslint:disable-next-line: typedef
+  ngOnInit() {
+    this.initializeUploader();
+  }
+
+  public fileOverBase(e: any): void {
+    this.hasBaseDropZoneOver = e;
+  }
+
+  // tslint:disable-next-line: typedef
+  initializeUploader() {
+    this.uploader = new FileUploader({
+      url: this.baseUrl + 'users/' + this.authService.decodedToken.nameid + '/photos',
+      authToken: 'Bearer ' + localStorage.getItem('token'),
+      isHTML5: true,
+      allowedFileType: ['image'],
+      removeAfterUpload: true,
+      autoUpload: false,
+      maxFileSize: 10 * 1024 * 1024
+    });
+
+    this.uploader.onAfterAddingFile = (file) => {file.withCredentials = false; };
+
+    this.uploader.onSuccessItem = (item, response, status, headers) => {
+      if (response) {
+        const res: Photo = JSON.parse(response);
+        const photo = {
+          id: res.id,
+          url: res.url,
+          dateAdded: res.dateAdded,
+          description: res.description,
+          isMain: res.isMain
+        };
+        this.photo.push(photo);
+      }
+    };
+  }
 }
