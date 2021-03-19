eb573f1ff5df12690070f7a61bf77352ac755c73
Nenad Sandic <Nenad.Sandic@vanderlande.com>
2021-03-19 01:54:25 +0100

    Chapter 13 part 3: Using action filters

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -58,6 +58,7 @@ namespace DatingApp.API
                         ValidateAudience = false
                     };
                 });
+            services.AddScoped<LogUserActivity>();
             // services.AddCors(allowsites=>{allowsites.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin());
             // });
         }
