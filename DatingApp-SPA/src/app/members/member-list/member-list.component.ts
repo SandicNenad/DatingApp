5648e51dfa31a973aa4224ab537e43a2d7d5b63b
Nenad Sandic <Nenad.Sandic@vanderlande.com>
2021-03-29 17:24:44 +0200

    Chapter 14 part 7: Using NGX boostrap pagination

--- DatingApp-SPA/src/app/members/member-list/member-list.component.ts
+++ DatingApp-SPA/src/app/members/member-list/member-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { PaginatedResult, Pagination } from 'src/app/_models/pagination';
 import { User } from '../../_models/user';
 import { AlertifyService } from '../../_services/alertify.service';
 import { UserService } from '../../_services/user.service';
@@ -11,6 +12,7 @@ import { UserService } from '../../_services/user.service';
 })
 export class MemberListComponent implements OnInit {
   users: User[];
+  pagination: Pagination;
 
   constructor(private userService: UserService, private alertify: AlertifyService, private route: ActivatedRoute) { }
 
@@ -19,16 +21,25 @@ export class MemberListComponent implements OnInit {
     // tslint:disable-next-line: deprecation
     this.route.data.subscribe(data => {
       this.users = data['users'].result;
+      this.pagination = data['users'].pagination;
     });
   }
 
+  pageChanged(event: any): void {
+    this.pagination.currentPage = event.page;
+    console.log(this.pagination.currentPage);
+    this.loadUsers();
+  }
+
   // tslint:disable-next-line: typedef
-  // loadUsers(){
-  //   this.userService.getUsers().subscribe((users: User[]) => {
-  //     this.users = users;
-  //   }, error => {
-  //     this.alertify.error(error);
-  //   });
-  // }
+  loadUsers(){
+    // tslint:disable-next-line: deprecation
+    this.userService.getUsers(this.pagination.currentPage, this.pagination.itemsPerPage).subscribe((res: PaginatedResult<User[]>) => {
+      this.users = res.result;
+      this.pagination = res.pagination;
+    }, error => {
+      this.alertify.error(error);
+    });
+  }
 
 }
